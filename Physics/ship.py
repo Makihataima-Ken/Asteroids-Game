ddc0c7ae228fc548ea0c9f960cce38250de1402c
Ken <ahmadsoud63@gmail.com>
2024-06-18 13:44:13 +0300

    some more additions

---
+++ Physics/ship.py
@@ -0,0 +1,5 @@
+class Ship:
+    def __init__(self,X,Y,ANGLE):
+        self.X=X
+        self.Y=Y
+        self.ANGLE=ANGLE
--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 import pygame
 import time
 import random
+import math
 
-WIDTH,HIGHT=1000,800
+WIDTH,HIGHT=1000,700
 WINDOW=pygame.display.set_mode((WIDTH,HIGHT))
 pygame.display.set_caption("Asteroids")
 
 #Ship's properties
-SHIP_X,SHIP_Y=40,60
+SHIP_X,SHIP_Y=WIDTH // 2,HIGHT // 2
 SPEED_X,SPEED_Y=50,50
-SHIP_ANGLE=90
+SHIP_ANGLE=0
 
-circle_center = (WIDTH // 2, HIGHT // 2)
-radius = 50
+ship_circle_center = (SHIP_X,SHIP_Y)
+radius = 30
 def draw():
-    pygame.draw.circle(WINDOW,"blue",circle_center,radius)
+    ship = pygame.draw.circle(WINDOW,"blue",ship_circle_center,radius)
+    
+    
+    ship_circle_distance = 20
+
+    POINTER_X=SHIP_X + math.cos(SHIP_ANGLE) * ship_circle_distance
+    POINTER_Y= SHIP_Y + math.sin(SHIP_ANGLE) * ship_circle_distance
+    pointer_circle_center=(POINTER_X,POINTER_Y)
+    pygame.draw.circle(WINDOW,"yellow",pointer_circle_center,5)
+    
     pygame.display.update()
 
+def update(dt):
+    global SHIP_ANGLE
+    global SHIP_X
+    global SHIP_Y
+    global SPEED_X
+    global SPEED_Y
+    
+    TURNING_VEL = 10
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_RIGHT]:
+        SHIP_ANGLE += TURNING_VEL * dt
+    
+    if keys[pygame.K_LEFT]:
+        SHIP_ANGLE -= TURNING_VEL * dt
+        
+    SHIP_ANGLE %= 2 * math.pi
+    
+    if keys[pygame.K_UP]:
+        ship_speed = 100
+        SPEED_X += math.cos(SHIP_ANGLE) * ship_speed * dt
+        SPEED_Y += math.sin(SHIP_ANGLE) * ship_speed * dt
+
+    SHIP_X += SPEED_X * dt
+    SHIP_Y += SPEED_Y * dt
+    
 def main():
     run=True
     
@@ -29,7 +65,6 @@ def main():
             if event.type==pygame.QUIT:
                 run=False
                 break
-        keys=pygame.key.get_pressed()
         draw()
             
     pygame.quit()
