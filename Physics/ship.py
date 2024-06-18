2d43dad07d655818b8f7d1287a517acdc2687ee1
Ken <ahmadsoud63@gmail.com>
2024-06-18 15:21:48 +0300

    trying to oop the mess

--- Physics/ship.py
+++ Physics/ship.py
@@ -1,5 +1,35 @@
+import pygame
+import math
+
 class Ship:
-    def __init__(self,X,Y,ANGLE):
-        self.X=X
-        self.Y=Y
-        self.ANGLE=ANGLE
+    def __init__(self, x, y, angle,vel_x,vel_y):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.vel_x=vel_x
+        self.vel_y=vel_y
+
+    def draw(self, win, colour, radius):
+        pygame.draw.circle(win, colour, (self.x, self.y), radius)
+
+    def update(self,dt,HIGHT,WIDTH):
+        TURNING_VEL = 10
+        keys = pygame.key.get_pressed()
+    
+        if keys[pygame.K_RIGHT]:
+            self.angle += TURNING_VEL * dt
+    
+        if keys[pygame.K_LEFT]:
+            self.angle-= TURNING_VEL * dt
+         
+        self.angle %= 2 * math.pi
+    
+        if keys[pygame.K_UP]:
+            ship_speed = 100
+            self.vel_x += math.cos(self.angle) * ship_speed * dt
+            self.vel_y += math.sin(self.angle) * ship_speed * dt
+
+        self.x += self.vel_x* dt
+        self.y += self.vel_y * dt
+        self.x%=WIDTH
+        self.y%=HIGHT
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 import time
 import random
 import math
+import Physics
 
 WIDTH,HIGHT=1000,700
-WINDOW=pygame.display.set_mode((WIDTH,HIGHT))
+WIN=pygame.display.set_mode((WIDTH,HIGHT))
 pygame.display.set_caption("Asteroids")
+#BG=pygame.image.load("AsteroidsBackground.jpg")
 
 #Ship's properties
 SHIP_X,SHIP_Y=WIDTH // 2,HIGHT // 2
@@ -15,7 +17,10 @@ SHIP_ANGLE=0
 ship_circle_center = (SHIP_X,SHIP_Y)
 radius = 30
 def draw():
-    ship = pygame.draw.circle(WINDOW,"blue",ship_circle_center,radius)
+    #WIN.blit(BG,(0,0))
+    WIN.fill(0,0,0)
+    pygame.draw.circle(WIN,"blue",ship_circle_center,radius)
+    
     
     
     ship_circle_distance = 20
@@ -23,7 +28,7 @@ def draw():
     POINTER_X=SHIP_X + math.cos(SHIP_ANGLE) * ship_circle_distance
     POINTER_Y= SHIP_Y + math.sin(SHIP_ANGLE) * ship_circle_distance
     pointer_circle_center=(POINTER_X,POINTER_Y)
-    pygame.draw.circle(WINDOW,"yellow",pointer_circle_center,5)
+    pygame.draw.circle(WIN,"yellow",pointer_circle_center,5)
     
     pygame.display.update()
 
@@ -52,6 +57,8 @@ def update(dt):
 
     SHIP_X += SPEED_X * dt
     SHIP_Y += SPEED_Y * dt
+    SHIP_X%=WIDTH
+    SHIP_Y%=HIGHT
     
 def main():
     run=True
